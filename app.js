1dbb68b8ab1919cdb552d31c4368ea0c5da875b1
happygod119 <happ0983493655@gmail.com>
2022-04-13 17:47:56 +0800

    add login and register route and page

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
+const router = require("./routes");
 const bcrypt = require("bcryptjs");
 
 const app = express();
 const PORT = 3000;
+
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+
+app.use(router);
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
 });
