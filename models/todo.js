23ddbd7fbc915592c2eafde00172585afa710965
happygod119 <happ0983493655@gmail.com>
2022-04-14 12:54:45 +0800

    add relations between user and todo models

--- models/todo.js
+++ models/todo.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   Todo.associate = function (models) {
     // associations can be defined here
+    Todo.belongsTo(models.User);
   };
   return Todo;
 };
