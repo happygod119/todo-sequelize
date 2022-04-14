ebcd1ce67dff6c50258d9037ae1ea5444ade941c
happygod119 <happ0983493655@gmail.com>
2022-04-14 15:38:36 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
+
 const router = require("./routes");
 const usePassport = require("./config/passport"); 
-const bcrypt = require("bcryptjs");
+
 
 const app = express();
 const PORT = 3000;
@@ -23,6 +24,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 usePassport(app); 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 app.use(router);
 
 app.listen(PORT, () => {
