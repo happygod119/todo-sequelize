637f6e61eaf852bf99be5baf6fed0895558e8d77
happygod119 <happ0983493655@gmail.com>
2022-04-14 15:26:35 +0800

    add login feature via passport

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
 const router = require("./routes");
+const usePassport = require("./config/passport"); 
 const bcrypt = require("bcryptjs");
 
 const app = express();
@@ -9,10 +11,18 @@ const PORT = 3000;
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+usePassport(app); 
 app.use(router);
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const db = require("../../models");
 const Todo = db.Todo;
@@ -10,9 +11,13 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post("/login", (req, res) => {
-  res.send("login");
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+  })
+);
 
 router.get("/register", (req, res) => {
   res.render("register");
